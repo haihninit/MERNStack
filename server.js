fbf1a2d2387e75afb745627b6a72f194886912bf
haihn <haihn.labianlabs@gmail.com>
2018-12-27 20:58:41 +0700

    Connect to MongoDB

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,() => console.log(`Server started on port ${port}`));
