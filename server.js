17841b8ef5f4533b848af19834b21d2c54e08a90
haihn <haihn.labianlabs@gmail.com>
2018-12-27 21:25:26 +0700

    use routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const items = require('./routes/api/items');
 const app = express();
 
 // Bodyparser middleware
@@ -13,6 +13,9 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
